f72e8d34e1225672fed5100d85cb886d8f0e030c
Kananoro <dezzeelkanal@gmail.com>
2024-04-05 16:31:43 +0500

    feat: add oAuth

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useSearchParams } from "next/navigation";
 import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { CardWrapper } from "./card-wrapper";
@@ -21,6 +22,11 @@ import { login } from "@/actions/login";
 import { useState, useTransition } from "react";
 
 export const LoginForm = () => {
+	const searchParams = useSearchParams();
+	const urlError =
+		searchParams.get("error") === "OAuthAccountNotLinked"
+			? "Email already in use with different provider!"
+			: "";
 	const [error, setError] = useState<string | undefined>("");
 	const [success, setSuccess] = useState<string | undefined>("");
 	const [isPending, startTransition] = useTransition();
@@ -37,8 +43,9 @@ export const LoginForm = () => {
 
 		startTransition(() => {
 			login(values).then((data) => {
-				setError(data.error);
-				setSuccess(data.success);
+				setError(data?.error);
+				// TODO: add when we add 2FA
+				// setSuccess(data?.success);
 			});
 		});
 	};
@@ -89,7 +96,7 @@ export const LoginForm = () => {
 							)}
 						/>
 					</div>
-					<FormError message={error} />
+					<FormError message={error || urlError} />
 					<FormSuccess message={success} />
 					<Button disabled={isPending} type="submit" className="w-full">
 						Login
